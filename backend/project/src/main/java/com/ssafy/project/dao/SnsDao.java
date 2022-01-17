f382383f805282b8560e5d24e2603f2a35b7e3cd
최호준 <matt9504@naver.com>
2022-01-18 00:15:08 +0900

    .

---
+++ backend/project/src/main/java/com/ssafy/project/dao/SnsDao.java
@@ -0,0 +1,8 @@
+package com.ssafy.project.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class SnsDao {
+    
+}
---
+++ backend/project/src/main/java/com/ssafy/project/dto/SnsDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.project.dto;
+
+public class SnsDto {
+    private int snsNo;
+    private int userNo;
+    private String userNickname;
+    private String content;
+
+    private String userProfileImageUrl;
+
+    
+    
+    
+}
