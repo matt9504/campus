cabc4f1dc266bc0fabc2d10eec35465c14af897f
FreeBono <rhkrehd619@naver.com>
2022-01-20 01:03:47 +0900

    makeparty ~ing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,27 +2,11 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-              
+     
   </div>
   <router-view/>
 
-  <div class="modal" tabindex="-1">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <p>Modal body text goes here.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
+
 </template>
 
 <style lang="scss">
