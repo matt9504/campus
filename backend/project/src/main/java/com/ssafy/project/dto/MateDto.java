dd6126edffa6ea684d249a9bd68b6f864fd0d899
최호준 <matt9504@naver.com>
2022-01-26 01:53:24 +0900

    S06P12E102-72 feat:mate CRD

--- backend/project/src/main/java/com/ssafy/project/dto/MateDto.java
+++ backend/project/src/main/java/com/ssafy/project/dto/MateDto.java
@@ -1,21 +1,31 @@
 package com.ssafy.project.dto;
 
-// import java.time.LocalDateTime;
+import java.sql.Date;
+import java.time.LocalDateTime;
 
+
+import lombok.Data;
+
+@Data
 public class MateDto {
-    // private int mateNo;
-    // private int mateCampsite;
-    // private int lowestAge;
-    // private int highestAge;
-    // private String mateCampstyle;
-    // private String mateCampstart;
-    // //private String
-    
-    
+    private int mateNo;
+    private String mateTitle;
+    private String mateCampsite;
+    private int lowestAge;
+    private int highestAge;
+    private String mateContent;
+    private String mateCampstyle;
+    private Date mateCampstart;
+    private Date mateCampend;
+    private int userNo;
 
+    private LocalDateTime regDt;
 
+    private int fileId;
+    private String fileName;
+    private long fileSize;
+    private String fileContentType;
+	private String fileUrl;
 
-
-    // private LocalDateTime regDt;
     
 }
