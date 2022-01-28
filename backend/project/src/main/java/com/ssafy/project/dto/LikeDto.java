84cbc1c9d046860f8dd6e6e2a3859276f94fad7d
최호준 <matt9504@naver.com>
2022-01-28 09:57:00 +0900

    220128 push

--- backend/project/src/main/java/com/ssafy/project/dto/LikeDto.java
+++ backend/project/src/main/java/com/ssafy/project/dto/LikeDto.java
@@ -1,5 +1,7 @@
 package com.ssafy.project.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SnsImageDto {
-    private int snsImageNo;
-    private int snsNo;
-    private String snsImageUrl;
-    
+public class LikeDto {
+    private List<SnsDto> like;
+    private int likeCount;
 }
