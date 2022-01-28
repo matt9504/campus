801cb16bba0d7482f8e2d54a7bc3ced287cf318a
최호준 <matt9504@naver.com>
2022-01-28 09:56:44 +0900

    220128 push

--- backend/project/src/main/java/com/ssafy/project/controller/SnsController.java
+++ backend/project/src/main/java/com/ssafy/project/controller/SnsController.java
@@ -3,11 +3,12 @@ package com.ssafy.project.controller;
 
 import javax.servlet.http.HttpSession;
 
+import com.ssafy.project.dto.LikeDto;
 import com.ssafy.project.dto.SnsDto;
 import com.ssafy.project.dto.SnsParamDto;
-import com.ssafy.project.dto.SnsReplyDto;
 import com.ssafy.project.dto.SnsReplyResultDto;
 import com.ssafy.project.dto.SnsResultDto;
+import com.ssafy.project.service.LikeService;
 import com.ssafy.project.service.SnsReplyService;
 import com.ssafy.project.service.SnsService;
 
@@ -19,8 +20,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -43,7 +42,11 @@ public class SnsController {
     @Autowired
     SnsReplyService snsReplyService;
 
+    @Autowired
+    LikeService likeService;
+
     private static final int SUCCESS = 1;
+    private static final int FAIL = -1;
 
     
     // 리스트 생성
@@ -108,6 +111,23 @@ public class SnsController {
             return new ResponseEntity<SnsResultDto>(snsResultDto, HttpStatus.INTERNAL_SERVER_ERROR); // 에러
         }
     }
+    // sns detail
+    @GetMapping(value="/sns/{snsNo}")
+    private ResponseEntity<SnsResultDto> snsDetail(@PathVariable int snsNo, HttpSession session) throws Exception{
+        
+        SnsParamDto snsParamDto = new SnsParamDto();
+        snsParamDto.setSnsNo(snsNo);
+        //snsParamDto.setUserNo( ((UserDto) session.getAttribute("userDto")).getUserNo());
+
+        SnsResultDto snsResultDto = snsService.snsDetail(snsParamDto);
+
+        if( snsResultDto.getResult() == SUCCESS ){
+            return new ResponseEntity<SnsResultDto>(snsResultDto, HttpStatus.OK);// 성공
+        }else{
+            return new ResponseEntity<SnsResultDto>(snsResultDto, HttpStatus.INTERNAL_SERVER_ERROR); // 에러
+        }
+    }
+
     // sns 댓글 리스트 생성
     @GetMapping(value="/sns/reply/{snsNo}")
     public ResponseEntity<SnsReplyResultDto> snsReplyList(@PathVariable int snsReplyNo){
@@ -119,42 +139,42 @@ public class SnsController {
         }else{
             return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.INTERNAL_SERVER_ERROR);// 에러
         }
-
     }
+    
     // sns 댓글 입력
-    @PostMapping(value="/sns/reply")
-    public ResponseEntity<SnsReplyResultDto> snsReplyInsert(@RequestBody SnsReplyDto snsReplyDto, HttpSession session){
+    // @PostMapping(value="/sns/reply")
+    // public ResponseEntity<SnsReplyResultDto> snsReplyInsert(@RequestBody SnsReplyDto snsReplyDto, HttpSession session){
         
-        //UserDto userDto = (UserDto) session.getAttribute("userDto");
+    //     //UserDto userDto = (UserDto) session.getAttribute("userDto");
 
-        snsReplyDto.setUserNo(userDto.getUserNo());
+    //     snsReplyDto.setUserNo(userDto.getUserNo());
 
-        SnsReplyResultDto snsReplyResultDto = snsReplyService.snsReplyInsert(snsReplyDto);
+    //     SnsReplyResultDto snsReplyResultDto = snsReplyService.snsReplyInsert(snsReplyDto);
 
-        if(snsReplyResultDto.getResult() == SUCCESS){
-            return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.OK);// 성공
-        }else{
-            return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.INTERNAL_SERVER_ERROR);// 에러
-        }
+    //     if(snsReplyResultDto.getResult() == SUCCESS){
+    //         return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.OK);// 성공
+    //     }else{
+    //         return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.INTERNAL_SERVER_ERROR);// 에러
+    //     }
 
-    }
-    // sns 댓글 수정
-    @PutMapping(value="/sns/reply")
-    public ResponseEntity<SnsReplyResultDto> snsReplyUpdate(@RequestBody SnsReplyDto snsReplyDto, HttpSession session){
+    // }
+    // // sns 댓글 수정
+    // @PutMapping(value="/sns/reply")
+    // public ResponseEntity<SnsReplyResultDto> snsReplyUpdate(@RequestBody SnsReplyDto snsReplyDto, HttpSession session){
         
-        //UserDto userDto = (UserDto) session.getAttribute("userDto");
+    //     //UserDto userDto = (UserDto) session.getAttribute("userDto");
 
-        snsReplyDto.setUserNo(userDto.getUserNo());
+    //     snsReplyDto.setUserNo(userDto.getUserNo());
 
-        SnsReplyResultDto snsReplyResultDto = snsReplyService.snsReplyUpdate(snsReplyDto);
+    //     SnsReplyResultDto snsReplyResultDto = snsReplyService.snsReplyUpdate(snsReplyDto);
 
-        if(snsReplyResultDto.getResult() == SUCCESS){
-            return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.OK);// 성공
-        }else{
-            return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.INTERNAL_SERVER_ERROR);// 에러
-        }
+    //     if(snsReplyResultDto.getResult() == SUCCESS){
+    //         return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.OK);// 성공
+    //     }else{
+    //         return new ResponseEntity<SnsReplyResultDto>(snsReplyResultDto, HttpStatus.INTERNAL_SERVER_ERROR);// 에러
+    //     }
 
-    }
+    // }
 
     @DeleteMapping(value="/sns/reply/{snsReplyNo}")
     public ResponseEntity<SnsReplyResultDto> snsReplyDelete(@PathVariable(value="snsNo") int snsReplyNo){
@@ -168,5 +188,23 @@ public class SnsController {
         }
 
     }
+
+    
+    @PostMapping(value = "/sns/like/{snsNo}")
+    public ResponseEntity<Integer> likeInsert(int snsNo, int userNo){
+        if(likeService.likeInsert(snsNo, userNo) == SUCCESS){
+            return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<Integer>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/sns/like/{snsNo}")
+    public ResponseEntity<LikeDto> likeList(@PathVariable int snsNo){
+        LikeDto like = new LikeDto();
+        like.setLike(likeService.likeList(snsNo));
+        return new ResponseEntity<LikeDto>(like, HttpStatus.OK);
+    }
+    
 
 }
