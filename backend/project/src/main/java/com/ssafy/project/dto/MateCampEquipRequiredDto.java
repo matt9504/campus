713ea88f770109922a1edd03d7ef4e5fbb95af2f
최호준 <matt9504@naver.com>
2022-01-31 14:59:02 +0900

    feat : mate equip
    Former-commit-id: 1621bdb7fb8aee7ee7d22e643eb22c6fea7b374a

--- backend/project/src/main/java/com/ssafy/project/dto/MateCampEquipRequiredDto.java
+++ backend/project/src/main/java/com/ssafy/project/dto/MateCampEquipRequiredDto.java
@@ -5,15 +5,15 @@ import lombok.Data;
 @Data
 public class MateCampEquipRequiredDto {
     private int mateNo;
-    private int mRequire1;
-    private int mRequire2;
-    private int mRequire3;
-    private int mRequire4;
-    private int mRequire5;
-    private int mRequire6;
-    private int mRequire7;
-    private int mRequire8;
-    private int mRequire9;
-    private int mRequire10;
+    private int brazier;
+    private int burner;
+    private int chair;
+    private int icebox;
+    private int lantern;
+    private int powerstrip;
+    private int sleepingbag;
+    private int table;
+    private int tarp;
+    private int tent;
 
 }
