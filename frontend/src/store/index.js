bf68e0e5327be578d86e658e35034c96d5dd3dab
FreeBono <rhkrehd619@naver.com>
2022-02-03 14:36:00 +0900

    for rev
    Former-commit-id: 42cea6a54f347dc3b09831f625ad699e1dcfc327

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex";
 
 // import axios from "axios";
 import createPersistedState from 'vuex-persistedstate'
-// const SERVER_URL = `http://i6e102.p.ssafy.io`
+const SERVER_URL = `http://i6e102.p.ssafy.io`
 import axios from "axios";
 // const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 
