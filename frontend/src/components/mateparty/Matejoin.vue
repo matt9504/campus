5b99819faff654e3cd89c6940de7f525d0ae697b
FreeBono <rhkrehd619@naver.com>
2022-02-04 14:34:33 +0900

    login hot fix
    Former-commit-id: 0bb75797e526e75238b2b98af5af97d8dfc0a46d

---
+++ frontend/src/components/mateparty/Matejoin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+    <b-button
+      :class="visible ? null : 'collapsed'"
+      :aria-expanded="visible ? 'true' : 'false'"
+      aria-controls="collapse-4"
+      @click="visible = !visible"
+    >
+      Toggle Collapse
+    </b-button>
+    <b-collapse id="collapse-4" v-model="visible" class="mt-2">
+      <b-card><div style="float:center">
+      <input type="text" v-model="friendnum"><button @click="join">메이트 참가</button>
+    </div></b-card>
+    </b-collapse>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'Matejoin',
+  data() {
+    return {
+      visible : false,
+      abc : this.$store.state.userList,
+      bcd : this.$store.state.mateList
+    }
+  },
+  created(){
+    console.log(this.abc)
+    console.log(this.bcd)
+  }
+
+}
+</script>
+
+<style>
+
+</style>
